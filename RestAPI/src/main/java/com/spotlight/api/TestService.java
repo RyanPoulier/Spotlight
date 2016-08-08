85b4d62062ae3118b0c2b28d722257f89f1402f9
padmakaw <padmaka.wijayagoonawardena@trycake.com>
2016-08-08 18:46:38 +0530

    commit adding swagger

--- RestAPI/src/main/java/com/spotlight/api/TestService.java
+++ RestAPI/src/main/java/com/spotlight/api/TestService.java
@@ -1,5 +1,10 @@
 package com.spotlight.api;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -9,10 +14,16 @@ import javax.ws.rs.core.Response;
  */
 
 @Path("/")
+@Api(value = "/", description = "Test service!")
 public class TestService {
 
     @GET
     @Path("/ping")
+    @ApiOperation(value = "Service Ping",
+            notes = "This the service ping")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 500, message = "Something wrong in Server")})
     public Response ping() {
         return Response.status(200).entity("Hi there !").build();
     }
