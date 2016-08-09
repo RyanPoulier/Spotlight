b375d6a6c147474a1dd52b6fd2639162aa16619e
padmakaw <padmaka.wijayagoonawardena@trycake.com>
2016-08-09 19:38:31 +0530

    commit changes to IssueDaoImpl.java

--- RestAPI/src/main/java/com/spotlight/core/dao/impl/IssueDaoImpl.java
+++ RestAPI/src/main/java/com/spotlight/core/dao/impl/IssueDaoImpl.java
@@ -3,6 +3,9 @@ package com.spotlight.core.dao.impl;
 import com.spotlight.core.beans.Issue;
 import com.spotlight.core.dao.IssueDao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Padmaka on 8/8/16.
  */
@@ -11,6 +14,21 @@ public class IssueDaoImpl implements IssueDao {
 
     @Override
     public Issue saveIssue(Issue issue) {
+
+        Map<String, Object> issueDoc = new HashMap<>();
+        issueDoc.put("title", issue.getTitle());
+        issueDoc.put("description", issue.getDescription());
+        issueDoc.put("address", issue.getAddress());
+        issueDoc.put("longitude", issue.getLongitude());
+        issueDoc.put("latitude", issue.getLatitude());
+        issueDoc.put("votes", issue.getVotes());
+        issueDoc.put("modifiedTime", issue.getModifiedTime());
+        issueDoc.put("estimatedDates", issue.getEstimatedDates());
+        issueDoc.put("actualResolutionDate", issue.getActualResolutionDate());
+        issueDoc.put("closureDate", issue.getClosureDate());
+        issueDoc.put("closureRating", issue.getClosureRating());
+
+
         return null;
     }
 }
