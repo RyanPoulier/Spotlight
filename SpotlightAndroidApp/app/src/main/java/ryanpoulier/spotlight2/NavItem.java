357e53611ad03f23c6edbbbc59621ebb0eeabc79
Shamal Perera <uslperera@gmail.com>
2016-08-17 22:20:50 +0530

    Revert "Changed the name of the app"
    This reverts commit 920f9f62098ba6dd65e30c6a6832f4f39d45876b.

---
+++ SpotlightAndroidApp/app/src/main/java/ryanpoulier/spotlight2/NavItem.java
@@ -0,0 +1,16 @@
+package ryanpoulier.spotlight2;
+
+/**
+ * Created by shamal on 8/4/16.
+ */
+public class NavItem {
+    String mTitle;
+    String mSubtitle;
+    int mIcon;
+
+    public NavItem(String title, String subtitle, int icon) {
+        mTitle = title;
+        mSubtitle = subtitle;
+        mIcon = icon;
+    }
+}
